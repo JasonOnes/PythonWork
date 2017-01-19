74809d6071b7f52bebf7d7ffc68ddd5027118281
JasonOnes <jasonr.jones14@gmail.com>
2017-01-19 12:33:46 -0800

    thurs lunch

---
+++ phonebook.py
@@ -0,0 +1,52 @@
+def phonebook(p_book=None):
+    if p_book is None:
+        p_book = dict()
+
+    x = input("Do you want to: \n1. create a new contact\n"
+              "2. Retrieve and existing contact\n"
+              "3. Update a contact\n"
+              "4. Delete a contact\nor \'quit\'\n")
+    if x == "1":
+        handle = input("What's your handle partner? ")
+        real_name = input("And your real name? ")
+        age = input("How old are you {}? ".format(handle))
+        number = input("What's your number? ")
+        p_book[handle] = real_name, age, number
+
+        print(p_book)
+        #phonebook(p_book=p_book)
+    elif x == "2":
+        handle = input("Who are you trying to find? ")
+        y = p_book[handle]
+        print(y)
+        #phonebook(p_book=p_book)
+    elif x == "3":
+        y = input("What contact do you want to change?")
+        z = input("What do you want to change?\n1.{}'s real name?\n2.{}'s age?\n3.{}'s number?".format(y))
+        if z == "1":
+            new_name = input("What do you want to change name to? ")
+            p_book[y] = new_name
+            #change name
+        elif z == "2":
+            #change age
+        elif z == "3":
+            #change number
+        else:
+            print("Nice try!")
+        p_book[y] = z
+        print(p_book)
+
+    elif x == "4":
+        handle = input("What contact do you want to delete? ")
+        del p_book[handle]
+        print(p_book)
+        #phonebook(p_book=p_book)
+    elif x == "quit":
+        quit()
+
+    phonebook(p_book=p_book)
+
+
+
+phonebook()
+
--- voting_booth.py
+++ voting_booth.py
@@ -1,12 +1,44 @@
 
-def candidates():
-    candidates = []
-    x = input("Give me a list of names!: ")
-    while x != 'done':
-        candidates.append(x)
-    elif x == 'done':
-        print("Finally")
-    print(candidates)
-    return candidates
+def candidates(c=None):
+    if c == None:
+        c = list()
 
-candidates()
+    x = input("Give me a list of names!: ")
+    if x != 'done':
+        c.append(x)
+        print(c)
+        candidates(c)
+
+    elif x == 'done':
+        print(c)
+
+    return c
+
+#candidates()
+"""
+def voting_booth():
+    candidates_list = list()
+    for voter in booth:
+        candidates_list.append(candidates())
+        print(candidates_list)
+    cand_votes = {}
+    cand_votes(enumerate(candidates_list))
+    print(cand_votes)"""
+
+def voting_booth(num_voters):
+    cand_list = list()
+    print("Welcome, step right up!")
+    for voter in range(num_voters):
+        print("Next!")
+        cand_list.append(candidates())
+    print(cand_list)
+    return cand_list
+
+voting_booth(5)
+
+def election_results("county"):
+    "county" = int(input("How many voters in the county? ")
+    tally = dict(enumerate(set(voting_booth("county"))))
+    print tally
+
+election    
