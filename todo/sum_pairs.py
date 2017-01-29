3141405f9755e770faee334f9544105c6fdb4352
JasonOnes <jasonr.jones14@gmail.com>
2017-01-28 17:55:34 -0800

    O(1)? attempt at sum_pairs

--- todo/sum_pairs.py
+++ todo/sum_pairs.py
@@ -16,7 +16,7 @@ Go through the search list and find all pairs of numbers that would add together
 """
 
 # Nested list comprehension perhaps?
-
+"""
 def find_sum_pairs(num_list, sum_x):
     #m = [(a, b) for a, b in num_list if (a + b) == sum_x]
     pairs_list = list()
@@ -34,7 +34,7 @@ def find_sum_pairs(num_list, sum_x):
     print(pairs_list)
 
 find_sum_pairs([1, 2, 3, 4, 5, 7], 7)
-"""
+
 def find_sum_pairs(num_list, sum_x):
     pairs_list = list()
     for number in num_list:
@@ -45,3 +45,21 @@ def find_sum_pairs(num_list, sum_x):
 
 find_sum_pairs([1, 2, 3, 4, 5, 7], 7)
 """
+
+# Still doesn't pass all the doctests but O(1), I think
+def find_sum_pairs(num_list, sum_x):
+    pairs_list = list()
+    first_index = 0
+    second_index = -1
+    
+    for num in num_list:
+        if num_list[first_index] + num_list[second_index] > sum_x:
+            second_index -= 1
+        elif num_list[first_index] + num_list[second_index] < sum_x:
+            first_index += 1
+        elif num_list[first_index] + num_list[second_index] == sum_x:
+            pairs_list.extend([num_list[first_index], num_list[second_index]])
+            first_index += 1
+    print(pairs_list)
+
+find_sum_pairs([1, 2, 3, 4, 5, 7], 7)
