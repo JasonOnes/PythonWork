bb3d36bb6d4a2c8c3a29dc43e34fcde5db7d4e7f
JasonOnes <jasonr.jones14@gmail.com>
2017-03-18 16:59:13 -0700

    not sure

--- Warmups/medi.py
+++ Warmups/medi.py
@@ -1,23 +1,6 @@
 """find the median value in a list"""
 
-"""
-def median(x):
-    y = sorted(x)
-    l = len(y)
 
-    if l % 2 == 0:
-        z = (y[(l//2)] + y[(l//2)-1]) / 2
-
-    else:
-        n = l // 2
-        z = y[n]
-
-
-    print(z)
-    return z
-
-median([12, 3, 89, 6, 5, 34958, 44, 22, 3, 12])
-"""
 def median(x):
     y = sorted(x)
     l = len(y)
